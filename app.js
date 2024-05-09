93e66e93feb1c85902b0403511544106ec654985
RaviChowdary97 <aklvvsr1999@gmail.com>
2024-05-09 16:42:01 +0530

    solution

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const PORT = 3000;
 mongoose.connect(
   "mongodb+srv://chowdary93:Chowdary143@cluster0.karwxgg.mongodb.net/mentorship"
 );
+app.get("/", (req, res) => {
+  res.send("Welcome to the Mentorship API!");
+});
 
 app.use(bodyParser.json());
 app.use("/api", apiRoutes);
